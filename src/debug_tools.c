744f81c3b333447544b55c489b6d48d2efee5b92
Santiago Akle <tiagoakle@gmail.com>
2015-01-23 11:21:11 -0800

    error in debug

--- src/debug_tools.c
+++ src/debug_tools.c
@@ -2,18 +2,18 @@
 
 void allocateHistory(pwork *w)
 {
-	w->info->hist_presy = (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));	
-	w->info->hist_presz = (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));
-    w->info->hist_dres = (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));
-    w->info->hist_gres= (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));
-    w->info->hist_mu= (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));
-    w->info->hist_expmu= (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));
-    w->info->hist_sigma= (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));
-    w->info->hist_tau= (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));
-    w->info->hist_kappa= (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));
-    w->info->hist_hpresy= (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));    
-    w->info->hist_hpresz= (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));
-    w->info->hist_hdres= (pfloat*)MALLOC(w->stgs->maxit(sizeof(pfloat)));
+	w->info->hist_presy = (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));	
+	w->info->hist_presz = (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));
+    w->info->hist_dres = (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));
+    w->info->hist_gres= (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));
+    w->info->hist_mu= (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));
+    w->info->hist_expmu= (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));
+    w->info->hist_sigma= (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));
+    w->info->hist_tau= (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));
+    w->info->hist_kappa= (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));
+    w->info->hist_hpresy= (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));    
+    w->info->hist_hpresz= (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));
+    w->info->hist_hdres= (pfloat*)MALLOC(w->stgs->maxit*(sizeof(pfloat)));
 }
 
 void freeHistory(pwork *w)
